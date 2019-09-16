0f8d9433524c6469591a5011fc5e130bfa38a068
baldwinn20 <42782171+baldwinn20@users.noreply.github.com>
2019-09-16 15:37:37 -0700

    created clickfun.js to keep click function in a separate file

---
+++ hw3/public/javascripts/clickfun.js
@@ -0,0 +1,33 @@
+clickHandler = function( event ) {
+  /*do stuff*/
+  //creates dialog when the "order" button has been clicke
+         var thankyou = $("<p></p>").text("Thank you! Your order has been placed.");
+         var detailsHead = $("<p></p>").text("Order details:");
+         //Reads the textArea input
+         var specialText = $.trim($("#noteArea").val());
+         //Records which radio button is checked
+         var checkedButton = $("input[name='flavor']:checked").val();
+         //Records the number selected in the dropdown box
+         var numCakes = $( "#numCheesecakes option:selected" ).text();
+         //displays values from checkedButton and numCakes
+         var orderMessage = $("<p></p>").text("You ordered " + numCakes + " " +checkedButton + " cheesecakes.");
+
+         // Show alert if the textArea contains keyword "vegan"
+         if(specialText.indexOf("vegan") != -1){
+         alert("Warning! Cheesecake contains dairy!");
+         }
+
+         //removes the form section and button
+         $("#orderTable").remove();
+         $("#noteHead").remove();
+         $("#noteArea").remove();
+         $("#orderButton").remove();
+
+         //displays order details specified in the vars above
+         $("body").append(thankyou, detailsHead, orderMessage);
+         });
+         });
+}
+$(function () {
+  $("button").click(clickHandler);
+});
