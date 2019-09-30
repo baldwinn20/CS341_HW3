ba385ff90e448405bd2e6ed730927a57c7004496
baldwinn20 <42782171+baldwinn20@users.noreply.github.com>
2019-09-30 13:51:31 -0700

    Added orders.js and edited app.js

---
+++ hw3/orders.js
@@ -0,0 +1,10 @@
+//Nicholas Baldwin
+//30 September 2019
+
+function openWindow() {
+  var order0 = {topping: "cherry", quantity: "2"};
+  var order1 = {topping: "plain", quantity: "6"};
+  var order2 = {topping: "chocolate", quantity: "6"};
+  var orderArray = ["order0", "order1", "order2"];
+  return orderArray;
+}
