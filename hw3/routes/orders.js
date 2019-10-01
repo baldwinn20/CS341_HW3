a4694b31a9aca06feda29938cef1beff6de31086
baldwinn20 <42782171+baldwinn20@users.noreply.github.com>
2019-09-30 21:54:23 -0700

    Reformatted code

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -5,26 +5,24 @@ var express = require('express');
 var router = express.Router();
 
 //define the array of orders with fixed values
-orderArray =
-[
-  {
-    "topping" : "cherry",
-    "quantity" : "2",
-  },
-  {
-    "topping" : "plain",
-    "quantity" : "6",
-  },
-  {
-    "topping" : "chocolate",
-    "quantity" : "3",
-  }
+orderArray = [{
+		"topping": "cherry",
+		"quantity": "2",
+	},
+	{
+		"topping": "plain",
+		"quantity": "6",
+	},
+	{
+		"topping": "chocolate",
+		"quantity": "3",
+	}
 ];
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(orderArray);
+router.get('/', function (req, res, next) {
+	res.send(orderArray);
 });
 
 module.exports = router;
